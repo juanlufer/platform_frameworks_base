PotatoProject/frameworks_base

base: ls-clocks: Respect accenter colors
Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
Change-Id: I9d97128c9d495191195c742168e1c15ba56994dd

packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -22,6 +22,7 @@ import android.app.ActivityManager;
 import android.app.IActivityManager;
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.res.AccentUtils;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -358,17 +359,17 @@ public class KeyguardStatusView extends GridLayout implements
             mClockView.setFormat12Hour(Html.fromHtml("<strong>hh</strong><br>mm"));
             mClockView.setFormat24Hour(Html.fromHtml("<strong>kk</strong><br>mm"));
         } else if (mClockSelection == 5) {
-            mClockView.setFormat12Hour(Html.fromHtml("hh<br><font color=" + getResources().getColor(R.color.sammy_clock_accent) + ">mm</font>"));
-            mClockView.setFormat24Hour(Html.fromHtml("kk<br><font color=" + getResources().getColor(R.color.sammy_clock_accent) + ">mm</font>"));
+            mClockView.setFormat12Hour(Html.fromHtml("hh<br><font color=" + AccentUtils.getAccentColor(getResources().getColor(R.color.sammy_clock_accent)) + ">mm</font>"));
+            mClockView.setFormat24Hour(Html.fromHtml("kk<br><font color=" + AccentUtils.getAccentColor(getResources().getColor(R.color.sammy_clock_accent)) + ">mm</font>"));
         } else if (mClockSelection == 6) {
-            mClockView.setFormat12Hour(Html.fromHtml("<strong>hh</strong><br><font color=" + getResources().getColor(R.color.sammy_clock_accent) + ">mm</font>"));
-            mClockView.setFormat24Hour(Html.fromHtml("<strong>kk</strong><br><font color=" + getResources().getColor(R.color.sammy_clock_accent) + ">mm</font>"));
+            mClockView.setFormat12Hour(Html.fromHtml("<strong>hh</strong><br><font color=" + AccentUtils.getAccentColor(getResources().getColor(R.color.sammy_clock_accent)) + ">mm</font>"));
+            mClockView.setFormat24Hour(Html.fromHtml("<strong>kk</strong><br><font color=" + AccentUtils.getAccentColor(getResources().getColor(R.color.sammy_clock_accent)) + ">mm</font>"));
         } else if (mClockSelection == 7) {
-            mClockView.setFormat12Hour(Html.fromHtml("<font color='#454545'>hh</font><br><font color=" + getResources().getColor(R.color.sammy_clock_accent) + ">mm</font>"));
-            mClockView.setFormat24Hour(Html.fromHtml("<font color='#454545'>kk</font><br><font color=" + getResources().getColor(R.color.sammy_clock_accent) + ">mm</font>"));
+            mClockView.setFormat12Hour(Html.fromHtml("<font color='#454545'>hh</font><br><font color=" + AccentUtils.getAccentColor(getResources().getColor(R.color.sammy_clock_accent)) + ">mm</font>"));
+            mClockView.setFormat24Hour(Html.fromHtml("<font color='#454545'>kk</font><br><font color=" + AccentUtils.getAccentColor(getResources().getColor(R.color.sammy_clock_accent)) + ">mm</font>"));
         } else if (mClockSelection == 8) {
-            mClockView.setFormat12Hour(Html.fromHtml("<strong><font color='#454545'>hh</font></strong><br><font color=" + getResources().getColor(R.color.sammy_clock_accent) + ">mm</font>"));
-            mClockView.setFormat24Hour(Html.fromHtml("<strong><font color='#454545'>kk</font></strong><br><font color=" + getResources().getColor(R.color.sammy_clock_accent) + ">mm</font>"));
+            mClockView.setFormat12Hour(Html.fromHtml("<strong><font color='#454545'>hh</font></strong><br><font color=" + AccentUtils.getAccentColor(getResources().getColor(R.color.sammy_clock_accent)) + ">mm</font>"));
+            mClockView.setFormat24Hour(Html.fromHtml("<strong><font color='#454545'>kk</font></strong><br><font color=" + AccentUtils.getAccentColor(getResources().getColor(R.color.sammy_clock_accent)) + ">mm</font>"));
         } else if (mClockSelection == 9) {
             mTextClock.onTimeChanged();
         } else {

packages/SystemUI/src/com/android/keyguard/clocks/TypographicClock.java
@@ -2,6 +2,7 @@ package com.android.keyguard.clocks;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.res.AccentUtils;
 import android.content.res.Resources;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -65,7 +66,8 @@ public class TypographicClock extends TextView {
         mResources = context.getResources();
         mHours = mResources.getStringArray(R.array.type_clock_hours);
         mMinutes = mResources.getStringArray(R.array.type_clock_minutes);
-        mAccentColor = mResources.getColor(R.color.custom_text_clock_top_color, null);
+        mAccentColor = AccentUtils.getAccentColor(mResources
+                .getColor(R.color.custom_text_clock_top_color, null));
 
         fadeIn = new AlphaAnimation(0, 1);
         fadeIn.setInterpolator(new DecelerateInterpolator());

juanlufer/platform_frameworks_base

Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>
Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -22,6 +22,7 @@ import android.app.ActivityManager;
 import android.app.IActivityManager;
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.res.AccentUtils;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -356,17 +357,17 @@ public class KeyguardStatusView extends GridLayout implements
             mClockView.setFormat12Hour(Html.fromHtml("<strong>hh</strong><br>mm"));
             mClockView.setFormat24Hour(Html.fromHtml("<strong>kk</strong><br>mm"));
         } else if (mClockSelection == 5) {
-            mClockView.setFormat12Hour(Html.fromHtml("hh<br><font color=" + getResources().getColor(R.color.sammy_minutes_accent) + ">mm</font>"));
-            mClockView.setFormat24Hour(Html.fromHtml("kk<br><font color=" + getResources().getColor(R.color.sammy_minutes_accent) + ">mm</font>"));
+            mClockView.setFormat12Hour(Html.fromHtml("hh<br><font color=" + AccentUtils.getAccentColor(getResources().getColor(R.color.sammy_minutes_accent)) + ">mm</font>"));
+            mClockView.setFormat24Hour(Html.fromHtml("kk<br><font color=" + AccentUtils.getAccentColor(getResources().getColor(R.color.sammy_minutes_accent)) + ">mm</font>"));
         } else if (mClockSelection == 6) {
-            mClockView.setFormat12Hour(Html.fromHtml("<strong>hh</strong><br><font color=" + getResources().getColor(R.color.sammy_minutes_accent) + ">mm</font>"));
-            mClockView.setFormat24Hour(Html.fromHtml("<strong>kk</strong><br><font color=" + getResources().getColor(R.color.sammy_minutes_accent) + ">mm</font>"));
+            mClockView.setFormat12Hour(Html.fromHtml("<strong>hh</strong><br><font color=" + AccentUtils.getAccentColor(getResources().getColor(R.color.sammy_minutes_accent)) + ">mm</font>"));
+            mClockView.setFormat24Hour(Html.fromHtml("<strong>kk</strong><br><font color=" + AccentUtils.getAccentColor(getResources().getColor(R.color.sammy_minutes_accent)) + ">mm</font>"));
         } else if (mClockSelection == 7) {
-            mClockView.setFormat12Hour(Html.fromHtml("<font color='#454545'>hh</font><br><font color=" + getResources().getColor(R.color.sammy_minutes_accent) + ">mm</font>"));
-            mClockView.setFormat24Hour(Html.fromHtml("<font color='#454545'>kk</font><br><font color=" + getResources().getColor(R.color.sammy_minutes_accent) + ">mm</font>"));
+            mClockView.setFormat12Hour(Html.fromHtml("<font color='#454545'>hh</font><br><font color=" + AccentUtils.getAccentColor(getResources().getColor(R.color.sammy_minutes_accent)) + ">mm</font>"));
+            mClockView.setFormat24Hour(Html.fromHtml("<font color='#454545'>kk</font><br><font color=" + AccentUtils.getAccentColor(getResources().getColor(R.color.sammy_minutes_accent)) + ">mm</font>"));
         } else if (mClockSelection == 8) {
-            mClockView.setFormat12Hour(Html.fromHtml("<strong><font color='#454545'>hh</font></strong><br><font color=" + getResources().getColor(R.color.sammy_minutes_accent) + ">mm</font>"));
-            mClockView.setFormat24Hour(Html.fromHtml("<strong><font color='#454545'>kk</font></strong><br><font color=" + getResources().getColor(R.color.sammy_minutes_accent) + ">mm</font>"));
+            mClockView.setFormat12Hour(Html.fromHtml("<strong><font color='#454545'>hh</font></strong><br><font color=" + AccentUtils.getAccentColor(getResources().getColor(R.color.sammy_minutes_accent)) + ">mm</font>"));
+            mClockView.setFormat24Hour(Html.fromHtml("<strong><font color='#454545'>kk</font></strong><br><font color=" + AccentUtils.getAccentColor(getResources().getColor(R.color.sammy_minutes_accent)) + ">mm</font>"));
         } else if (mClockSelection == 9) {
             mTextClock.onTimeChanged();
         } else {

packages/SystemUI/src/com/android/keyguard/clocks/TypographicClock.java
@@ -3,6 +3,7 @@ package com.android.keyguard.clocks;
 import android.app.WallpaperManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.res.AccentUtils;
 import android.content.res.Resources;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -25,6 +26,7 @@ import com.android.internal.util.NotificationColorUtil;
 import com.android.keyguard.R;
 import com.android.systemui.Dependency;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
+import com.android.systemui.statusbar.policy.ConfigurationController;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -77,7 +79,8 @@ public class TypographicClock extends TextView implements ColorExtractor.OnColor
         mResources = context.getResources();
         mHours = mResources.getStringArray(R.array.type_clock_hours);
         mMinutes = mResources.getStringArray(R.array.type_clock_minutes);
-        mSystemAccent = mResources.getColor(R.color.custom_text_clock_top_color, null);
+        mSystemAccent = AccentUtils.getAccentColor(mResources
+                .getColor(R.color.custom_text_clock_top_color, null));
         mFallbackColor = mResources.getColor(R.color.custom_text_clock_top_fallback_color, null);
         onColorsChanged(mColorExtractor, 0);
     }