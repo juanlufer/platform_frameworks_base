i-o-n/frameworks_base

Accent Tinted and blurred filter for media coverart [1/2]

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1907,6 +1907,11 @@ public class StatusBar extends SystemUI implements DemoMode,
                     case 4:
                         artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), ImageHelper.getGrayscaleBlurredImage(mContext, artworkBitmap, 7.0f));
                         break;
+                    case 5:
+                        Drawable aww = new BitmapDrawable(mBackdropBack.getResources(), artworkBitmap);
+                        Bitmap aaww = ImageHelper.getColoredBitmap(aww, mContext.getResources().getColor(R.color.coverart_accent));
+                        artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), ImageHelper.getBlurredImage(mContext, aaww, 7.0f));
+                        break;
                     case 0:
                     default:
                         artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), artworkBitmap);

juanlufer/platform_frameworks_base

Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1929,6 +1929,11 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                     case 4:
                         artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), ImageHelper.getGrayscaleBlurredImage(mContext, artworkBitmap, 25.0f));
                         break;
+                    case 5:
+                        Drawable aww = new BitmapDrawable(mBackdropBack.getResources(), artworkBitmap);
+                        Bitmap aaww = ImageHelper.getColoredBitmap(aww, mContext.getResources().getColor(R.color.sammy_minutes_accent));
+                        artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), ImageHelper.getBlurredImage(mContext, aaww, 7.0f));
+                        break;
                     case 0:
                     default:
                         artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), artworkBitmap);